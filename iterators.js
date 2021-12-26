d67802be5c74fea5f0e0d15931aa0a11e200c692
Bader Mohsen <bader.an.mohsen@gmail.com>
2021-12-26 15:56:39 +0300

    Iterators task

--- iterators.js
+++ iterators.js
@@ -4,9 +4,8 @@
  * - Accepts an array
  * - Logs every element of the array
  ************************************/
-const logger = function (array) {
-
-};
+const logger = (array) => array.forEach(element => console.log(element))
+ 
 
 /*************************************
  * toCelsius(temperatures)
@@ -19,10 +18,7 @@ const logger = function (array) {
  * The conversion is:
  *   C = (F - 32) * (5/9)
  ************************************/
-const toCelsius = function (temperatures) {
-
-};
-
+const toCelsius = (temperatures) =< temperatures.map(temp)
 /**************************************
  * hottestDays(temperatures, threshhold)
  *
@@ -31,9 +27,8 @@ const toCelsius = function (temperatures) {
  * - Returns an array of temperatures
  *   that exceed the threshhold
  ***************************************/
-const hottestDays = function (temperatures, threshhold) {
-
-};
+const hottestDays =  (temperatures, threshhold) => temperatures.filter(element => element > threshold)
+;
 
 /******************************************
  * logHottestDays(temperatures, threshhold)
@@ -50,13 +45,13 @@ const hottestDays = function (temperatures, threshhold) {
  *       all previous functions
  *******************************************/
 const logHottestDays = function (temperatures, threshhold) {
-
+logger(toCelsius(hottestDays(temperatures,threshold)))
 };
 
 /* Uncomment the following lines to test your code... */
 
-// logger([1, 2, 3, 4, 5, 6, 7]);
-// console.log(toCelsius([212, 122])); // should be: [100, 50];
+logger([1, 2, 3, 4, 5, 6, 7]);
+console.log(toCelsius([212, 122])); // should be: [100, 50];
 // console.log(hottestDays([1, 2, 3, 4, 5, 6, 7, 8, 13, 156, 1765], 5)); // should be : [6, 7, 8, 13, 156, 1765]
 // logHottestDays([140, 68, 212, 45, 149, 122, 19], 80); // should log "60", "100", "65", and "50"
 
